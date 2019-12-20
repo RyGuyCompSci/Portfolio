c6a86df4437c1cbefb4ad008c733fe261f457820
Ryan Jones <ryan_jones5@taylor.edu>
2019-12-20 10:58:56 -0500

    Consolidated and added functions to assembler

--- MIPS-Assembler/assemblr.c
+++ MIPS-Assembler/assemblr.c
@@ -1,5 +1,5 @@
 /***
- * Assembler for MIPS-32 
+ * Assembler for MIPS-32
  * Created for COS 381 - Computer Architecture
  *
  * @author Ryan Jones
@@ -9,19 +9,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define MAX_LENGTH 1024
+
+FILE *fp;
+int display_format = 0, debug = 0, geisler = 0;
 
 // helper commands
 void print_bits(unsigned bits);
 void parse_command(char *str);
+void write_to_file(int n);
 int translate_register(char *reg);
 int masked_value(int shamt, int shval);
 void shift_value(int *value, int shamt, int shval);
 void get_r_type_values(int *rd, int *rs, int *rt);
-void get_i_type_values(int *rs, int *rt, int *immediate); 
+void get_i_type_values(int *rs, int *rt, int *immediate);
 void r_type_shift(int *instruction, int rs, int rt, int rd);
 void i_type_shift(int *instruction, int rs, int rt, int immediate);
+void usage();
 
 // conversion of MIPS instructions
 void add(int rd, int rs, int rt);
@@ -45,7 +51,16 @@ void ori(int rs, int rt, int immediate);
 void slt(int rd, int rs, int rt);
 void slti(int rs, int rt, int immediate);
 void sltiu(int rs, int rt, int immediate);
-void nop ();
+void sltu(int rd, int rs, int rt);
+void sll(int rd, int rt, int shamt);
+void srl(int rd, int rt, int shamt);
+void sb(int rs, int rt, int immediate);
+void sc(int rs, int rt, int immediate);
+void sh(int rs, int rt, int immediate);
+void sw(int rs, int rt, int immediate);
+void sub(int rd, int rs, int rt);
+void subu(int rs, int rt, int immediate);
+void xor(int rd, int rs, int rt);
 
 
 //////////////////////////////////////////////////////////////////////
@@ -59,9 +74,7 @@ void nop ();
  */
 int masked_value(int shamt, int shval) {
     int mask = 0x01;
-    for(int i = 0; i < shamt; i++) {
-        mask <<= 2;
-    }
+	mask <<= shamt;
     mask -= 1;
     return (shval & mask);
 }
@@ -80,12 +93,24 @@ void shift_value(int *value, int shamt, int shval) {
  * Helpful for debugging
  * www.geeksforgeeks.org/binary-representation-of-a-given-number/
  */
-void print_bits(unsigned n) 
-{ 
-    unsigned i; 
-    for (i = 1 << 31; i > 0; i = i / 2) 
-        (n & i)? printf("1"): printf("0"); 
-    printf("\n");
+void print_bits(unsigned n)
+{
+    if(debug) {
+        if(display_format) {
+            printf("0x%08x\n", n);
+        } else {
+            unsigned i;
+            for (i = 1 << 31; i > 0; i = i / 2)
+            (n & i)? printf("1"): printf("0");
+            printf("\n");
+        }
+    }
+    write_to_file((int) n);
+}
+
+
+void write_to_file(int n) {
+    if(fp != NULL) fwrite(&n, 4, 1, fp);
 }
 
 
@@ -105,7 +130,12 @@ void get_r_type_values(int *rd, int *rs, int *rt) {
 void get_i_type_values(int *rt, int *rs, int *immediate) {
 	*rt = translate_register(strtok(NULL, ", "));
 	*rs = translate_register(strtok(NULL, ", "));
-	*immediate = atoi(strtok(NULL, " \n\r"));
+    char *imm = strtok(NULL, " \n\r");
+    if(strlen(imm) > 1 && !strncmp(imm, "0x", 2)) {
+        *immediate = strtol(imm, NULL, 16);
+    } else {
+        *immediate = atoi(imm);
+    }
 }
 
 
@@ -166,7 +196,7 @@ int translate_register(char *reg) {
 	if(!strcmp(reg, "$sp"))   return 29;
 	if(!strcmp(reg, "$fp"))   return 30;
 	if(!strcmp(reg, "$ra"))   return 31;
-	//else return atoi(reg + 1); // returns numeric value (e.g.: $17)
+	else return atoi(reg); // returns numeric value (e.g.: $17)
 }
 
 
@@ -221,7 +251,7 @@ void addu(int rd, int rs, int rt) {
  */
 void addiu(int rs, int rt, int immediate) {
 	int instruction = 0;
-	
+
 	shift_value(&instruction, 6, 0x09);
 	i_type_shift(&instruction, rs, rt, immediate);
 
@@ -247,7 +277,7 @@ void and(int rd, int rs, int rt) {
  */
 void andi(int rs, int rt, int immediate) {
 	int instruction = 0;
-	
+
 	shift_value(&instruction, 6, 0x0c);
 	i_type_shift(&instruction, rs, rt, immediate);
 
@@ -262,7 +292,7 @@ void beq(int rs, int rt, int immediate) {
 	int instruction = 0;
 
 	shift_value(&instruction, 6, 0x04);
-	i_type_shift(&instruction, rs, rt, immediate);
+	i_type_shift(&instruction, rs, rt, (immediate >> 2));
 
 	print_bits(instruction);
 }
@@ -275,7 +305,7 @@ void bne(int rs, int rt, int immediate) {
 	int instruction = 0;
 
 	shift_value(&instruction, 6, 0x05);
-	i_type_shift(&instruction, rs, rt, immediate);
+	i_type_shift(&instruction, rs, rt, (immediate >> 2));
 
 	print_bits(instruction);
 }
@@ -286,9 +316,9 @@ void bne(int rs, int rt, int immediate) {
  */
 void j(int address) {
 	int instruction = 0;
-	
+
 	shift_value(&instruction, 6, 0x02);
-	shift_value(&instruction, 26, address);
+	shift_value(&instruction, 26, (address >> 2));
 
 	print_bits(instruction);
 }
@@ -301,7 +331,7 @@ void jal(int address) {
 	int instruction = 0;
 
 	shift_value(&instruction, 6, 0x03);
-	shift_value(&instruction, 26, address);
+	shift_value(&instruction, 26, (address >> 2));
 
 	print_bits(instruction);
 }
@@ -326,7 +356,7 @@ void jr(int rs) {
  */
 void lbu(int rs, int rt, int immediate) {
 	int instruction = 0;
-	
+
 	shift_value(&instruction, 6, 0x24);
 	i_type_shift(&instruction, rs, rt, immediate);
 
@@ -340,7 +370,7 @@ void lbu(int rs, int rt, int immediate) {
  */
 void lhu(int rs, int rt, int immediate) {
 	int instruction = 0;
-	
+
 	shift_value(&instruction, 6, 0x25);
 	i_type_shift(&instruction, rs, rt, immediate);
 
@@ -369,7 +399,7 @@ void lui(int rs, int rt, int immediate) {
 
 	shift_value(&instruction, 6, 0x0f);
 	i_type_shift(&instruction, rs, rt, immediate);
-	
+
 	print_bits(instruction);
 }
 
@@ -384,14 +414,6 @@ void lw(int rs, int rt, int immediate) {
     i_type_shift(&instruction, rs, rt, immediate);
 
     print_bits(instruction);
-}
-
-
-/**
- * Prints all bit-zeroes
- */
-void nop() {
-    printf("%032d\n", 0);
 }
 
 
@@ -473,6 +495,142 @@ void sltiu(int rs, int rt, int immediate) {
 }
 
 
+/**
+ * Generates the sltu command
+ */
+void sltu(int rd, int rs, int rt) {
+	int instruction = 0;
+
+	r_type_shift(&instruction, rs, rt, rd);
+	shift_value(&instruction, 6, 0x2b);
+
+	print_bits(instruction);
+}
+
+
+/**
+ * Generates the sll command
+ */
+void sll(int rd, int rt, int shamt) {
+    int instruction = 0;
+
+    shift_value(&instruction, 5, rt);
+    if(geisler) shift_value(&instruction, 5, 0x0);
+    shift_value(&instruction, 5, rd);
+    shift_value(&instruction, 5, shamt);
+    shift_value(&instruction, 6, 0x0);
+
+    print_bits(instruction);
+}
+
+
+/**
+ * Generates the srl command
+ */
+void srl(int rd, int rt, int shamt) {
+    int instruction = 0;
+
+    shift_value(&instruction, 5, rt);
+    if(geisler) shift_value(&instruction, 5, 0x0);
+    shift_value(&instruction, 5, rd);
+    shift_value(&instruction, 5, shamt);
+    shift_value(&instruction, 6, 0x02);
+
+    print_bits(instruction);
+}
+
+
+/**
+ * Generates the sb command
+ */
+void sb(int rs, int rt, int immediate) {
+    int instruction = 0;
+
+    shift_value(&instruction, 6, 0x28);
+    i_type_shift(&instruction, rs, rt, immediate);
+
+    print_bits(instruction);
+}
+
+
+/**
+ * Generates the sc command
+ */
+void sc(int rs, int rt, int immediate) {
+    int instruction = 0;
+
+    shift_value(&instruction, 6, 0x38);
+    i_type_shift(&instruction, rs, rt, immediate);
+
+    print_bits(instruction);
+}
+
+
+/**
+ * Generates the sh command
+ */
+void sh(int rs, int rt, int immediate) {
+    int instruction = 0;
+
+    shift_value(&instruction, 6, 0x29);
+    i_type_shift(&instruction, rs, rt, immediate);
+
+    print_bits(instruction);
+}
+
+
+/**
+ * Generates the sw command
+ */
+void sw(int rs, int rt, int immediate) {
+    int instruction = 0;
+
+    shift_value(&instruction, 6, 0x2b);
+    i_type_shift(&instruction, rs, rt, immediate);
+
+    print_bits(instruction);
+}
+
+
+/**
+ * Generates the sub command
+ */
+void sub(int rd, int rs, int rt) {
+	int instruction = 0;
+
+	r_type_shift(&instruction, rs, rt, rd);
+	shift_value(&instruction, 6, 0x22);
+
+	print_bits(instruction);
+}
+
+
+/**
+ * Generates the subu command
+ */
+void subu(int rd, int rs, int rt) {
+	int instruction = 0;
+
+	r_type_shift(&instruction, rs, rt, rd);
+	shift_value(&instruction, 6, 0x23);
+
+	print_bits(instruction);
+}
+
+
+/**
+ * Generates the xor command
+ */
+void xor(int rd, int rs, int rt) {
+    int instruction = 0;
+
+    r_type_shift(&instruction, rs, rt, rd);
+    shift_value(&instruction, 6, 0x26);
+
+    print_bits(instruction);
+}
+
+
 //////////////////////////////////////////////////////////////////////
 //     					   PARSE OF COMMAND                         //
 //////////////////////////////////////////////////////////////////////
@@ -483,7 +641,7 @@ void sltiu(int rs, int rt, int immediate) {
  */
 void parse_command(char *str) {
 	char *op = strtok(str, " \n\r");
-
+    if(!op || !strncmp(op, "#", 1)) return; // newline or comment
 	// variables to work with
 	int address, immediate, rs, rt, rd, shamt;
 
@@ -491,52 +649,66 @@ void parse_command(char *str) {
 		get_r_type_values(&rd, &rs, &rt);
 		add(rd, rs, rt);
 	}
-	
+
 	else if(!strcmp(op, "addi")) {
 		get_i_type_values(&rt, &rs, &immediate);
 		addi(rs, rt, immediate);
 	}
-	
+
 	else if(!strcmp(op, "addu")) {
-		get_r_type_values(&rd, &rs, &rt);	
+		get_r_type_values(&rd, &rs, &rt);
 		addu(rd, rs, rt);
 	}
-	
+
 	else if(!strcmp(op, "addiu")) {
 		get_i_type_values(&rt, &rs, &immediate);
 		addiu(rs, rt, immediate);
 	}
-	
+
 	else if(!strcmp(op, "and")) {
 		get_r_type_values(&rd, &rs, &rt);
 		and(rd, rs, rt);
 	}
-	
+
 	else if(!strcmp(op, "andi")) {
 		get_i_type_values(&rt, &rs, &immediate);
 		andi(rs, rt, immediate);
 	}
-	
+
 	else if(!strcmp(op, "beq")) {
 		get_i_type_values(&rt, &rs, &immediate);
 		beq(rs, rt, immediate);
 	}
-	
+
 	else if(!strcmp(op, "bne")) {
 		get_i_type_values(&rt, &rs, &immediate);
 		bne(rs, rt, immediate);
 	}
-	
+
+    else if(!strcmp(op, "halt")) {
+        print_bits(0xffffffff);
+    }
+
 	else if(!strcmp(op, "j")) {
-		address = atoi(strtok(NULL, "\n\r"));
+        char *addr = strtok(NULL, "\n\r");
+        if(!strncmp(addr, "0x", 2)) {
+            address = strtol(addr, NULL, 16);
+        } else {
+    		address = atoi(addr);
+        }
 		j(address);
 	}
-	
+
 	else if(!strcmp(op, "jal")) {
-		address = atoi(strtok(NULL, "\n\r"));
+        char *addr = strtok(NULL, "\n\r");
+        if(!strncmp(addr, "0x", 2)) {
+            address = strtol(addr, NULL, 16);
+        } else {
+    		address = atoi(addr);
+        }
 		jal(address);
 	}
-	
+
 	else if(!strcmp(op, "jr")) {
 		rs = translate_register(strtok(NULL, "\n\r"));
 		jr(rs);
@@ -564,13 +736,18 @@ void parse_command(char *str) {
 
 	else if(!strcmp(op, "lw")) {
 		rt = translate_register(strtok(NULL, ", "));
-		immediate = atoi(strtok(NULL, "("));
+        char *imm = strtok(NULL, "(");
+        if(strlen(imm) > 2 && !strncmp(imm, "0x", 2)) {
+            immediate = (int)strtol(imm, NULL, 0);
+        } else {
+    		immediate = atoi(imm);
+        }
 		rs = translate_register(strtok(NULL, ")"));
 		lw(rs, rt, immediate);
 	}
-	
+
 	else if(!strcmp(op, "nop")) {
-		nop();
+		print_bits(0x0);
 	}
 
 	else if(!strcmp(op, "nor")) {
@@ -603,13 +780,82 @@ void parse_command(char *str) {
 		sltiu(rs, rt, immediate);
 	}
 
-	else if(!strcmp(op, "sll")) {
-		
+	else if(!strcmp(op, "sltu")) {
+		get_r_type_values(&rd, &rs, &rt);
+		sltu(rd, rs, rt);
 	}
-	
+
+	else if(!strcmp(op, "sll")) {
+        get_r_type_values(&rd, &rs, &shamt);
+        sll(rd, rs, shamt);
+	}
+
+	else if(!strcmp(op, "srl")) {
+        get_r_type_values(&rd, &rs, &shamt);
+        srl(rd, rs, shamt);
+	}
+
+    else if(!strcmp(op, "sb")) {
+		rt = translate_register(strtok(NULL, ", "));
+		immediate = atoi(strtok(NULL, "("));
+		rs = translate_register(strtok(NULL, ")"));
+		sb(rs, rt, immediate);
+    }
+
+    else if(!strcmp(op, "sc")) {
+		rt = translate_register(strtok(NULL, ", "));
+		immediate = atoi(strtok(NULL, "("));
+		rs = translate_register(strtok(NULL, ")"));
+		sc(rs, rt, immediate);
+    }
+
+    else if(!strcmp(op, "sh")) {
+		rt = translate_register(strtok(NULL, ", "));
+		immediate = atoi(strtok(NULL, "("));
+		rs = translate_register(strtok(NULL, ")"));
+		sh(rs, rt, immediate);
+    }
+
+    else if(!strcmp(op, "sw")) {
+		rt = translate_register(strtok(NULL, ", "));
+        char *imm = strtok(NULL, "(");
+        if(strlen(imm) > 1 && !strncmp(imm, "0x", 2)) {
+            immediate = (int)strtol(imm, NULL, 0);
+        } else {
+    		immediate = atoi(imm);
+        }
+		rs = translate_register(strtok(NULL, ")"));
+		sw(rs, rt, immediate);
+    }
+
+    else if(!strcmp(op, "sub")) {
+        get_r_type_values(&rd, &rs, &rt);
+        sub(rd, rs, rt);
+    }
+
+    else if(!strcmp(op, "subu")) {
+        get_r_type_values(&rd, &rs, &rt);
+        subu(rd, rs, rt);
+    }
+
+    else if(!strcmp(op, "xor")) {
+        get_r_type_values(&rd, &rs, &rt);
+        xor(rd, rs, rt);
+    }
+
 	else { // handles unknown commands
 		printf("Unknown command: %s\n", op);
+        usage();
 	}
+}
+
+
+void usage() {
+    printf("\n-d : show debug output\n");
+    printf("-b : show output as bits [default]\n");
+    printf("-h : show output as hex\n");
+    printf("-o : write values to 0x1000\n");
+    printf("-g : accounts for Geisler's implementation\n\n");
 }
 
 
@@ -617,6 +863,40 @@ void parse_command(char *str) {
  * Main function of the program
  */
 int main(int argc, char **argv, char **envp) {
+
+    int c;
+    int start_at_thousand = 0;
+    while((c = getopt(argc, argv, "bdhog")) != -1) {
+        switch(c) {
+            case 'b':
+                display_format = 0;
+                break;
+            case 'd':
+                debug = 1;
+                break;
+            case 'h':
+                display_format = 1;
+                break;
+            case 'o':
+                start_at_thousand = 1;
+                break;
+            case 'g':
+                geisler = 1;
+                break;
+            defualt:
+                break;
+        }
+    }
+
+    fp = fopen("data.bin", "w");
+
+    if(start_at_thousand) {
+        int n = 0;
+        for(int i = 0; i < 0x1000; i+=4) {
+            fwrite(&n, 4, 1, fp);
+            n++;
+        }
+    }
 
 	char input[MAX_LENGTH];
 	while(fgets(input, MAX_LENGTH, stdin) != NULL) {
